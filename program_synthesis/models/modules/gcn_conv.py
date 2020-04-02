688b8a938f9ee7853d5618f0e13f26b6b47ce8ad
Kavi Gupta <kavi@berkeley.edu>
2020-04-01 21:38:59 -0700

    Add gated graph convnet

--- program_synthesis/models/modules/gcn_conv.py
+++ program_synthesis/models/modules/gcn_conv.py
@@ -1,4 +1,5 @@
 import torch_geometric
+import torch
 import torch.nn as nn
 
 class GCNConv(torch_geometric.nn.MessagePassing):
@@ -55,3 +56,50 @@ class MultiGCNConv(nn.Module):
         if len(self.convs) > 0:
             vertices = self.convs[-1](vertices, edges)
         return vertices
+
+class GGCNLayer(nn.Module):
+    """
+    Gated Graph Convnet as described in https://arxiv.org/pdf/1711.00740.pdf
+    """
+    def __init__(self, dim, n_edge_types):
+        super().__init__()
+        self.dim = dim
+        self.n_edge_types = n_edge_types
+
+        self.emb_to_message = nn.Linear(self.dim, self.dim * self.n_edge_types)
+        self.recurrent = nn.GRU(self.dim, self.dim)
+
+    def forward(self, embedding, edges):
+        """
+        m[n][e] == the nth vertex's eth edge type's emdedding
+
+        q = [m[u][e] : (u, v, e) in edges]
+
+        r[i] == the ith vertex's result emdedding
+
+        r[v] = g({m[u][e] : (u, v, e) in edges})
+             = g({q[i] : edges[1][i]})
+
+        r = 0
+        r.scatter_add_(0, index, q)
+
+        where index[i][k] = ev[i]
+        """
+        assert len(embedding.shape) == 2
+        assert len(edges.shape) == 2 and edges.shape[0] == 3
+        e_v = edges[1]
+        e_ue = edges[[0, 2]]
+
+        m = self.emb_to_message(embedding)
+        m = m.reshape(m.shape[0], self.n_edge_types, self.dim)
+
+        q = m[e_ue.tolist()]
+
+        index = e_v.unsqueeze(-1).repeat(1, self.dim)
+
+        r = torch.zeros_like(embedding)
+        r.scatter_add_(0, index, q)
+
+        _, new_embedding = self.recurrent(r, embedding)
+
+        return new_embedding
