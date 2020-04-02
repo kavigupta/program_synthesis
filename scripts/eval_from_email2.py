1870abd0b536303c60458ca4c8f802e8d0b54d16
Kavi Gupta <kavi@berkeley.edu>
2020-04-02 04:39:05 -0700

    Add real

--- scripts/eval_from_email2.py
+++ scripts/eval_from_email2.py
@@ -15,9 +15,11 @@ for logdir in glob.glob('logdirs/**/*', recursive=True):
         cm100 = ckpt_number - 100
         if (cm100 % 50000 != 0 and cm100 % 10000 == 0) or ckpt_number < 1000:
             continue
-        for dist in ('1', '0,1', '0,0,1'):
+        for dist in ('1', '0,1', '0,0,1', ''):
             for on_train in True, False:
-                mode = 'train' if on_train else 'eval'
+                if on_train and dist == '':
+                    continue
+                mode = 'train' if on_train else ('eval' if dist else 'real')
                 extra_args = '--eval-train --limit 2500' if on_train else ''
                 output_path = "{logdir}/report-dev-m{dist}-{step}-{mode}.jsonl".format(logdir=logdir, dist=dist, step=ckpt_number, mode=mode)
                 if os.path.exists(output_path):
@@ -26,10 +28,15 @@ for logdir in glob.glob('logdirs/**/*', recursive=True):
                     if res.get('total', 0) >= 2500:
                         # print(output_path, file=sys.stderr)
                         continue
+                if dist:
+                    mutate_or_ref = '--karel-mutate-ref --karel-mutate-n-dist {dist} '.format(dist=dist)
+                else:
+                    mutate_or_ref = '--karel-file-ref-val ../nearai/logdirs/baseline_model/on-val.json'
+
                 print(
                     ('python -u program_synthesis/eval.py --model_type karel-lgrl-ref '
                      '--dataset karel --max_beam_trees 64 --step {step} '
-                     '--karel-mutate-ref --karel-mutate-n-dist {dist} '
+                     '{mutate_or_ref} '
                      '--model_dir {logdir} '
                      '--batch_size 64 '
                      '--report-path {output_path} '
@@ -38,8 +45,9 @@ for logdir in glob.glob('logdirs/**/*', recursive=True):
                      # '--restore-map-to-cpu'
                     ).format(
                         step=ckpt_number, logdir=logdir,
-                        dist=dist, output_path=output_path,
-                        extra_args=extra_args
+                        extra_args=extra_args,
+                        output_path=output_path,
+                        mutate_or_ref = mutate_or_ref
                     )
                 )
 
