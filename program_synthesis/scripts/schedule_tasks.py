9f71b4e48c9e07be0e27984c231410a809059f45
Kavi Gupta <kavi@berkeley.edu>
2020-04-19 17:39:37 -0700

    replenish with cmd line args

--- program_synthesis/scripts/schedule_tasks.py
+++ program_synthesis/scripts/schedule_tasks.py
@@ -15,19 +15,20 @@ def run_on_gpu(gpu, task):
     command = "CUDA_VISIBLE_DEVICES={} {}".format(gpu, task)
     file = tempfile.TemporaryFile()
     proc = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=file, shell=os.environ['SHELL'])
-    return command, file, proc
+    return command, task, file, proc
 
 class TaskRunner:
-    def __init__(self, tasks, *, max_memory, max_procs):
+    def __init__(self, tasks, *, max_memory, max_procs, command_line_replenish):
         self.current_processes = {}
         self.pbar = tqdm.tqdm(total=len(tasks))
         self.tasks = tasks[::-1] # reverse to ensure we do the first task first when we run tasks.pop()
 
         self.max_memory = max_memory
         self.max_procs = max_procs
+        self.command_line_replenish = command_line_replenish
 
     def handle_done(self):
-        for gpu, (name, file, proc) in list(self.current_processes.items()):
+        for gpu, (name, task, file, proc) in list(self.current_processes.items()):
             retcode = proc.poll()
             if retcode is not None:
                 if retcode != 0:
@@ -60,14 +61,29 @@ class TaskRunner:
     def run_all(self):
         try:
             for _ in every(1):
+                if len(self.tasks) < 10:
+                    self.replenish_tasks()
                 if not (self.tasks or self.current_processes):
                     break
                 self.step()
         finally:
             self.pbar.close()
             print("Terminating")
-            for _, _, proc in tqdm.tqdm(list(self.current_processes.values())):
+            for _, _, _, proc in tqdm.tqdm(list(self.current_processes.values())):
                 proc.kill()
+
+    def replenish_tasks(self):
+        if self.command_line_replenish is None:
+            return
+        with tempfile.TemporaryFile() as tmp:
+            proc = subprocess.Popen(self.command_line_replenish, stdout=tmp, shell=os.environ['SHELL'])
+            proc.wait()
+            tmp.seek(0)
+            new_tasks = [x.strip() for x in tmp.read().decode('utf-8').split("\n")[::-1] if x.strip()]
+            current_command_lines = {task for _, task, _, _ in self.current_processes.values()} | set(self.tasks)
+            new_tasks = [x for x in new_tasks if x not in current_command_lines]
+            self.tasks = new_tasks + self.tasks
+            self.pbar.total += len(new_tasks)
 
 def run_tasks(tasks, **kwargs):
     TaskRunner(tasks, **kwargs).run_all()
@@ -78,6 +94,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--max-memory", type=float, default=1.0)
     parser.add_argument("--max-procs", type=int, default=float('inf'))
+    parser.add_argument("--command-line-replenish", type=str)
     args = parser.parse_args()
 
-    run_tasks(list(stdin), max_memory=args.max_memory, max_procs=args.max_procs)
+    run_tasks(list(stdin), max_memory=args.max_memory, max_procs=args.max_procs, command_line_replenish=args.command_line_replenish)
