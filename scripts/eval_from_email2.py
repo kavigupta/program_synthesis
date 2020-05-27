77021e213db34efd484184b801925722f121992e
Kavi Gupta <kavi@berkeley.edu>
2020-05-26 18:28:00 -0700

    Fix bug

--- scripts/eval_from_email2.py
+++ scripts/eval_from_email2.py
@@ -223,7 +223,7 @@ def main(args):
             )
 
             command += extra_args + ' '
-            search_param = ''
+            model_data = search_param = ''
             if mode == 'real' or mode == 'realtrain':
                 model_data, search_param = param
                 command += '--karel-file-ref-val baseline/{}-val.json'.format(model_data)
