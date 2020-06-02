43638823d686a285eb6390c594c41c3e1b817bb7
Kavi Gupta <kavi@berkeley.edu>
2020-06-01 18:30:27 -0700

    change set of mutations

--- program_synthesis/analysis/models_to_analyze.py
+++ program_synthesis/analysis/models_to_analyze.py
@@ -3,9 +3,14 @@ vanilla
 vanilla-123-old
 vanilla-123-old-real-nearai-finetuned-1e-4
 vanilla-123-old-real-nearai32-finetuned-1e-4
+vanilla-123-old-finetuned-agg-start-with-finetuned
+vanilla-1-old
+vanilla-1-old-real-nearai-finetuned-1e-4
 
-aggregate-standard
 aggregate-with-io
+# aggregate-with-io-123-old
+# aggregate-with-io-123-old-real-nearai-finetuned-1e-4
+# aggregate-with-io-123-old-real-nearai32-finetuned-1e-4
 
 vanilla-real-nearai-finetuned-1e-4
 aggregate-with-io-real-nearai-finetuned-1e-4
@@ -13,8 +18,8 @@ aggregate-with-io-real-nearai-finetuned-1e-4
 # vanilla-real-nearai-finetuned-rl-1e-5
 # aggregate-with-io-real-nearai-finetuned-rl-1e-5
 
-# vanilla-many-mutations
-# aggregate-with-io-many-mutations
+vanilla-many-mutations
+aggregate-with-io-many-mutations
 # vanilla-real-nearai-finetuned-rl-1e-5-use-heldout
 # aggregate-with-io-real-nearai-finetuned-rl-1e-5-use-heldout
 
