1e19b033d645837f658e2d1bd6ee569ac381ba4d
Kavi Gupta <kavi@berkeley.edu>
2020-08-11 02:54:11 -0700

    different models

--- program_synthesis/analysis/models_to_analyze.py
+++ program_synthesis/analysis/models_to_analyze.py
@@ -1,11 +1,11 @@
 model_labels = """
 vanilla
-vanilla-123-old
-vanilla-123-old-real-nearai-finetuned-1e-4
-vanilla-123-old-real-nearai32-finetuned-1e-4
-vanilla-123-old-finetuned-agg-start-with-finetuned
-vanilla-1-old
-vanilla-1-old-real-nearai-finetuned-1e-4
+#vanilla-123-old
+#vanilla-123-old-real-nearai-finetuned-1e-4
+#vanilla-123-old-real-nearai32-finetuned-1e-4
+#vanilla-123-old-finetuned-agg-start-with-finetuned
+#vanilla-1-old
+#vanilla-1-old-real-nearai-finetuned-1e-4
 
 aggregate-with-io
 # aggregate-with-io-123-old
@@ -28,5 +28,9 @@ aggregate-with-io-many-mutations
 
 vanilla-real-nearai32-finetuned-1e-4
 aggregate-with-io-real-nearai32-finetuned-1e-4
+
+vanilla,h,327
+vanilla,h,327,finetuned-nearai-1e-4
+vanilla,h,327,finetuned-nearai32-1e-4
 """.strip().split("\n")
 model_labels = [x for x in model_labels if x and x[0] != '#']
